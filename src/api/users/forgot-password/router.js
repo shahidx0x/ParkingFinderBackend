77b09ec61f572b810bd170ed3a3106b1f0d3471d
shahidx0x <shahidabdulla.aas@gmail.com>
2023-04-21 00:02:38 +0600

    fix swagger-ui css problem on vercel

--- src/api/users/forgot-password/router.js
+++ src/api/users/forgot-password/router.js
@@ -3,11 +3,7 @@ const { forgot_password, verifyToken, resetPassword } = require("./controller");
 
 const forgot_pass_router = require("express").Router();
 
-forgot_pass_router.post(
-  "/user/forgot-password",
-  authenticator,
-  forgot_password
-);
+forgot_pass_router.post("/user/forgot-password", forgot_password);
 forgot_pass_router.post("/user/verifyToken", verifyToken);
 forgot_pass_router.post("/user/resetPassword", resetPassword);
 
